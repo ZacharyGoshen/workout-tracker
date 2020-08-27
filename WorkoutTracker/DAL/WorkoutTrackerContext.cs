636eda6747872b6dda15b77627bb85b1aa4fb420
Zachary Goshen <zacharygoshen@Zacharys-MacBook-Air.local>
2020-08-27 09:44:41 -0400

    Add connection to RDS MySQL DB

---
+++ WorkoutTracker/DAL/WorkoutTrackerContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WorkoutTracker.DAL
+{
+    public partial class WorkoutTrackerContext : DbContext
+    {
+        public WorkoutTrackerContext()
+        {
+        }
+
+        public WorkoutTrackerContext(DbContextOptions<WorkoutTrackerContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("data source=workout-tracker-database.cuchqo5r0bsj.us-east-1.rds.amazonaws.com;initial catalog=workout_tracker_database;user id=admin;password=password", x => x.ServerVersion("8.0.17-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
